3838119c8f9c62b5314083ea940e0e2c92997b8c
Serhii Yevchishen <magodza06@gmail.com>
2023-06-03 01:21:19 +0300

    add ImageGallery.styled.

--- src/components/ImageGallery/ImageGallery.styled.js
+++ src/components/ImageGallery/ImageGallery.styled.js
@@ -6,8 +6,8 @@ export const ImageGalleryUl = styled.ul`
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-gap: 16px;
   margin: 0px auto;
-
-  padding: 0px;
+  // padding-top: 70px;
+  padding: 90px 0px 20px;
   list-style: none;
   box-sizing: inherit;
 `;
